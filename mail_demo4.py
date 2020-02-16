bf80a9a8cd7f7e57bdad887d448995a0127a7082
Tobi Mesubi <49198254+tobimesh@users.noreply.github.com>
2020-02-16 23:51:16 +0000

    Update mail_demo4.py

--- mail_demo4.py
+++ mail_demo4.py
@@ -9,8 +9,8 @@ EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
 msg = EmailMessage()
 msg['Subject'] = 'Check out the network topolgy'
-msg['From'] = 'tobimesh@gmail.com'
-msg['To'] = 'tobi@info-mesh.co.uk'
+msg['From'] = 'toegegwbw@gmail.com'
+msg['To'] = 'topewvevnevv@invneveesh.co.uk'
 msg.set_content('Image attached...')
 
 
@@ -28,10 +28,10 @@ for file in files:
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 
 	# smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-	smtp.login("tobimesh@gmail.com", "letmein")
+	smtp.login("tevobivfbe@gmail.com", "letmein")
 
 	# smtp.sendmail(SENDER, RECEIVER, msg)
 
 	
-	# smtp.sendmail(EMAIL_ADDRESS, 'tobi@info-mesh.co.uk', msg) 
+	# smtp.sendmail(EMAIL_ADDRESS, 'teveeweem@ccikh.co.uk', msg) 
 	smtp.send_message(msg)
