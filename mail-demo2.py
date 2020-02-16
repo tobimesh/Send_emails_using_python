a1258a7c51eecbc8ba779607bccf78dbffebeab7
Tobi Mesubi <49198254+tobimesh@users.noreply.github.com>
2020-02-16 23:49:05 +0000

    Update mail-demo2.py

--- mail-demo2.py
+++ mail-demo2.py
@@ -8,18 +8,18 @@ EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
 msg = EmailMessage()
 msg['Subject'] = 'Grab dinner this weekend?'
-msg['From'] = 'tobimesh@gmail.com'
-msg['To'] = 'tobi@info-mesh.co.uk'
+msg['From'] = 'tobofbefe@gmail.com'
+msg['To'] = 'tobi@igeveenfve.co.uk'
 msg.set_content('How about dinner at 6pm this saturday?')
 
 # added SSL and removed the smtp.ehlo()
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 
 	# smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-	smtp.login("tobimesh@gmail.com", "letmein")
+	smtp.login("tobieovbev@gmail.com", "letmein")
 
 	# smtp.sendmail(SENDER, RECEIVER, msg)
 
 	
-	# smtp.sendmail(EMAIL_ADDRESS, 'tobi@info-mesh.co.uk', msg) 
+	# smtp.sendmail(EMAIL_ADDRESS, 'tobi@ioebve.co.uk', msg) 
 	smtp.send_message(msg)
